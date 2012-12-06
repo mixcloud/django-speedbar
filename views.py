8e4f0420763392ba151ccb49f1e97e6129c61d80
Theo Spears <theo@theos.me.uk>
2012-12-06 17:16:23 +0000

    Restrict the detail endpoints to staff and debug

--- views.py
+++ views.py
@@ -1,22 +1,26 @@
 from django.http import HttpResponse
 from django.core.cache import cache
-
+from django.conf import settings
+from mixcloud.utils.decorators import staff_only
 from mixcloud.speedbar.utils import DETAILS_PREFIX, TRACE_PREFIX
+
 
 import json
 import time
 
+@staff_only
 def panel(request, trace_id):
     details = cache.get(DETAILS_PREFIX + trace_id)
-    if details:
+    if details and settings.DEBUG:
         details_json = json.dumps(details, skipkeys=True, default=repr, indent=2)
         return HttpResponse(content=details_json)
     else:
         return HttpResponse(status=404)
 
+@staff_only
 def trace(request, trace_id):
     trace = cache.get(TRACE_PREFIX + trace_id)
-    if trace:
+    if trace and settings.DEBUG:
         trace_json = json.dumps(trace, indent=2)
         return HttpResponse(content=trace_json, mimetype="application/json")
     else:
