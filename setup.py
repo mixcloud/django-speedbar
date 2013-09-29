586b38d25837f9a687a597466c9048868aeb5627
Theo Spears <theo@theos.me.uk>
2013-09-29 17:13:15 +0100

    Add python package metadata

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-speedbar',
+    version='0.1',
+    packages=['speedbar'],
+    include_package_data=True,
+    license='MIT License',
+    description='Provides a break down of page loading time',
+    long_description=README,
+    url='http://github.com/theospears/django-speedbar',
+    author='Theo Spears',
+    author_email='theo@mixcloud.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
