b01108d82549651f5b20846c04366fcba3b990c5
Theo Spears <theo@theos.me.uk>
2013-10-06 22:20:00 +0100

    Fix setup.py to install sub-packages

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-speedbar',
     version='0.1',
-    packages=['speedbar'],
+    packages = find_packages(),
     include_package_data=True,
     license='MIT License',
     description='Provides a break down of page loading time',
