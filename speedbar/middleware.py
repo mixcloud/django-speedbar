494854cf3f5bf0b8672e350137edae8fb1d6b911
Theo Spears <theo@theos.me.uk>
2014-02-25 18:01:59 +0000

    Move intial setup into middleware file
    This will get loaded earlier than models for WSGI workers. In particular
    it will not be loaded between a request starting and finishing.

--- speedbar/middleware.py
+++ speedbar/middleware.py
@@ -1,25 +1,52 @@
-from speedbar.modules.base import RequestTrace
+"""
+Speedbar module
 
-from django.utils.encoding import smart_unicode, smart_str
-from django.utils.html import escapejs
-from django.core.urlresolvers import reverse
-from django.conf import settings
+This provides performance metrics, details of operations performed, and Chrome SpeedTracer integration
+for page loads.
 
+Information is provided by a set of modules, which are responsible for recording and reporting data.
+The collected data is then collected and made available via template tags, headers, and a HAR file.
+
+On startup each module is given a chance to initialize itself, typically this consists of monkey
+patching a set of built in django functionality. A per request module object is created in response
+to the start of each request. Over the course of the request modules record data, using thread local
+storage to associate correctly with the right request. A middleware then writes out summary data,
+and the headers required to fetch more detailed information from the server. Finally the request_finished
+signal handler stores detailed information to memcache which can then be retrieved.
+
+"""
 import re
 
-HTML_TYPES = ('text/html', 'application/xhtml+xml')
+from django.conf import settings
+from django.core.signals import request_started, request_finished
+from django.core.urlresolvers import reverse
+from django.utils.encoding import smart_unicode, smart_str
 
+from speedbar.signals import setup_request_tracing, store_request_trace
+from speedbar.utils import init_modules
+from speedbar.modules.base import RequestTrace
+
+
+if getattr(settings, 'SPEEDBAR_ENABLE', True):
+    # We hook everything up in the middleware file as loading the middleware is one of the first things performed
+    # by the django WSGI implementation.
+    init_modules()
+
+    request_started.connect(setup_request_tracing, dispatch_uid='request_started_speedbar_setup_request_tracing')
+    request_finished.connect(store_request_trace, dispatch_uid='request_started_speedbar_store_request_trace')
+
+
+HTML_TYPES = ('text/html', 'application/xhtml+xml')
 METRIC_PLACEHOLDER_RE = re.compile('<span data-module="(?P<module>[^"]+)" data-metric="(?P<metric>[^"]+)"></span>')
 
-SPEEDBAR_ENABLE = getattr(settings, 'SPEEDBAR_ENABLE', True)
 
 class SpeedbarMiddleware(object):
     def process_request(self, request):
-        if SPEEDBAR_ENABLE:
+        if getattr(settings, 'SPEEDBAR_ENABLE', True):
             RequestTrace.instance().stacktracer.root.label = '%s %s' % (request.method, request.path)
 
     def process_response(self, request, response):
-        if not SPEEDBAR_ENABLE:
+        if not getattr(settings, 'SPEEDBAR_ENABLE', True):
             return response
 
         request_trace = RequestTrace.instance()
@@ -61,7 +88,7 @@ class SpeedbarMiddleware(object):
         Adds all summary metrics to the response headers, so they can be stored in nginx logs if desired.
         """
         def sanitize(string):
-            return string.title().replace(' ','-')
+            return string.title().replace(' ', '-')
 
         for module, module_values in metrics.items():
             for key, value in module_values.items():
