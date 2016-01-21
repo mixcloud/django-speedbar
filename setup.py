d30397e5b597aad4590981f9ac741f9f0bb87300
Mat Clayton <mat@wakari.co.uk>
2016-01-21 00:56:00 +0000

    start of 1.9 support

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-speedbar',
-    version='0.2.0',
+    version='0.3.0',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
@@ -32,6 +32,6 @@ setup(
     ],
     install_requires=[
         'ProxyTypes>=0.9',
-        'Django >=1.5, <1.9',
+        'Django >=1.5, <1.10',
     ],
 )
