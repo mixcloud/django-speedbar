a5c94035c74d6c1a153b22287dcc28d92e486b68
Mat Clayton <mat@mixcloud.com>
2016-01-22 14:08:14 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-speedbar',
-    version='0.3.0',
+    version='0.2.1',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
